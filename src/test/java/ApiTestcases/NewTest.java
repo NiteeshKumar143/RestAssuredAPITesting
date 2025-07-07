6b29c8969b8bb61a02243c9635a450b67995d61c
Niteeshkumarapp <niteesh.kumar@appinventiv.com>
2025-07-08 00:21:19 +0530

    second commit

--- src/test/java/ApiTestcases/NewTest.java
+++ src/test/java/ApiTestcases/NewTest.java
@@ -6,6 +6,10 @@ public class NewTest
     public static void main(String[] args)
     {
         System.out.println("Only for testing purpose");
+        System.out.println("Only for testing purpose");
+        System.out.println("Only for testing purpose");
+        System.out.println("Only for testing purpose");
+        
     }
 
 }
